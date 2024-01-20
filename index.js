ba6984523d70276fd4179565f5c806a3a4fa5c0b
Jezer1991 <jezerjjv@gmail.com>
2024-01-21 00:23:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,28 @@ app.get("/status", (request, response) => {
     response.status(200).send({msg:"mensaje"})
 });
 
+app.get('/api/tests', (req, res) => {
+    const sql = "SELECT " +
+        " t.id as id_test," +
+        " t.nombre as nombre_test," +
+        " t.id_bloque," +
+        " t.id_tipo_test," +
+        " tt.nombre as nombre_tipo_test," +
+        " b.nombre as nombre_bloque," +
+        " te.nombre_corto as nombre_corto_tema," +
+        " te.nombre_largo as nombre_largo_tema" +
+        " FROM test t" +
+        " INNER JOIN tipo_test tt on t.id_tipo_test = tt.id" +
+        " INNER JOIN bloque b on b.id = t.id_bloque" +
+        " INNER JOIN tema te on te.id = t.id_tema";
+    db.query(sql, async (err, result) => {
+        if (err === null) {
+            res.send({ code: 201, result });
+        } else {
+            res.send({ code: 202, err });
+        }
+    });
+})
 
 app.listen(PORT, () => {
     console.log(`Server corriendo en ${PORT}`);
