77c2a5939a2accd26f0d66e12d2458d203aa4152
Jezer1991 <jezerjjv@gmail.com>
2024-01-21 00:22:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
+const { query } = require("express");
+const db = require("./db");
+const multer = require('multer')
+const path = require('path');
+const fs = require('fs');
+var url = require('url');
+const dir = "../../../../../../../xampp/htdocs/";
+const axios = require('axios');
+const dotenv = require('dotenv');
+const webpack = require('webpack');
+const http = require('http');
+
 const PORT = process.env.PORT || 3977;
 
 
-
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, dir + 'dbImagenes/kairos/productos')))
 
 
 app.get("/status", (request, response) => {
